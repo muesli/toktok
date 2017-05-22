9fd4629c375f24c05a06ba1541432fea20c3ce26
Christian Muehlhaeuser <muesli@gmail.com>
2017-05-22 21:15:48 +0200

    Updated example & README

--- examples/tokens.go
+++ examples/tokens.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/muesli/toktok"
 )
 
@@ -10,6 +11,7 @@ func main() {
 	bucket, _ := toktok.NewBucket(8)
 
 	// Generate a bunch of tokens with a safety distance of 4
+	// Distance is calculated by insertion cost (1), deletion cost (1) and substitution cost (2)
 	for i := 0; i < 10; i++ {
 		fmt.Println("Generated Token:", bucket.NewToken(4).Code)
 	}
