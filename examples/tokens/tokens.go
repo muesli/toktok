29f4f9414ce35e06dd6857059ae76b1678d8925c
Christian Muehlhaeuser <muesli@gmail.com>
2022-02-03 00:33:20 +0100

    Update example

--- examples/tokens/tokens.go
+++ examples/tokens/tokens.go
@@ -7,22 +7,24 @@ import (
 )
 
 func main() {
-	// Generate a new token bucket. Each generated token will be 8 characters long
+	// Generate a new token bucket.
+	// Each generated token will be 8 characters long.
 	bucket, _ := toktok.NewBucket(8)
 
-	// Generate a bunch of tokens with a safety distance of 4
-	// Distance is calculated by insertion cost (1), deletion cost (1) and substitution cost (2)
+	// Generate a bunch of tokens with a safety distance of 4.
+	// Distance is calculated by insertion cost (1), deletion cost (1) and
+	// substitution cost (2).
 	for i := 0; i < 9; i++ {
 		token, _ := bucket.NewToken(4)
 		fmt.Printf("Generated Token %d: %s\n", i, token)
 	}
 
-	// One more token that we will tamper with
+	// One more token that we will tamper with.
 	token, _ := bucket.NewToken(4)
 	fmt.Printf("Generated Token 9: %s\n", token)
 	token = "_" + token[1:7] + "_"
 
-	// Find the closest match for the faulty token
+	// Find the closest match for the faulty token.
 	match, distance := bucket.Resolve(token)
 	fmt.Printf("Best match for '%s' is token '%s' with distance %d\n", token, match, distance)
 }
